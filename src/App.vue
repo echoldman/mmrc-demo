c03ac1df7f253a204ad720d43f9bb6c3ef672a74
db <db@local>
2022-02-01 11:50:56 +0800

    Add the mmrc

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,23 @@
 </template>
 
 <script>
+import { ipcRenderer } from 'electron'
 import HelloWorld from './components/HelloWorld.vue'
+
+const MMRCRenderer = require('mmrc/renderer')
+const mmrc = new MMRCRenderer('mmrc.main.call.method', 'mmrc.renderer.done', 'mmrc.renderer.failed', ipcRenderer)
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted () {
+    mmrc.C('hello', 'renderer call main method.').then(message => {
+      console.log(message)
+    }).catch(error => {
+      console.log(error)
+    })
   }
 }
 </script>
